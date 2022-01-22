68f2b521dcc6f6825513df9e5377d6a4eb3a6a30
Julian Baumgartner <jbaumgartner93@gmail.com>
2022-01-22 12:45:54 -0500

    Update install scirpt

--- install.sh
+++ install.sh
@@ -1,11 +1,22 @@
 #! /bin/bash
 
+# If virtualenv not installed, download & install it for user
+if ! python3 -c "import virtualenv" &> /dev/null; then
+    python3 -m pip install --user virtualenv
+fi
+
+# Create  virtual env for python dependencies
 virtualenv venv
 source venv/bin/activate
 
+# Install briefcase
 pip install briefcase
 
+# Build app template
 briefcase build
+
+# Package app template to build disk image
 briefcase package --no-sign
 
+# Open disk image to promp user and install
 open macOS/LockIn-0.0.1.dmg
