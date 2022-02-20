a8b6e30a05bccebe2da9aef1e7d1fa9b94bde163
Julian Baumgartner <jbaumgartner93@gmail.com>
2022-02-20 15:45:42 -0500

    Update readme and install

--- install.sh
+++ install.sh
@@ -1,5 +1,21 @@
 #! /bin/bash
 
+# Location of lockin settings file
+SETTINGS_FILE="src/lockin/settings.py"
+
+#User input
+read -p "Please enter your name: " HOST_NAME
+read -r -p "Please enter network shared folder location. Example: /Volumes/NAS/: " NETWORK_SHARE
+
+# Target strings to up date in settings file
+HOST_TARGET_KEY="HOST"
+NET_SHARE_TARGET_KEY="NETWORK_SHARE_URI"
+
+# Replace strings in settings file
+sed -i -e "s|\($HOST_TARGET_KEY *= *\).*|\1'$HOST_NAME'|" $SETTINGS_FILE
+sed -i -e "s|\($NET_SHARE_TARGET_KEY *= *\).*|\1'$NETWORK_SHARE'|" $SETTINGS_FILE
+
+
 # If virtualenv not installed, download & install it for user
 if ! python3 -c "import virtualenv" &> /dev/null; then
     python3 -m pip install --user virtualenv
