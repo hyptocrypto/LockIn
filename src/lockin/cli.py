773a2856c7c99be2e26b6a2f4ff8202c474e6df8
Julian Baumgartner <julianbaumgartner@Julians-MacBook-Air.local>
2022-08-30 23:34:00 -0400

    CLI poc working

---
+++ src/lockin/cli.py
@@ -0,0 +1,51 @@
+import subprocess
+from prompt_toolkit import prompt
+from prompt_toolkit.completion import WordCompleter
+from manager import CredentialsManager
+from styles import CliStyles
+from exceptions import ServiceNotFound
+
+
+def copy_to_clipboard(txt):
+    task = subprocess.Popen(["pbcopy"], stdin=subprocess.PIPE, close_fds=True)
+    task.communicate(input=txt.encode("utf-8"))
+
+
+def fetch_service(client: CredentialsManager):
+    services = WordCompleter(client.list_services(), ignore_case=True)
+    chosen_service = prompt(CliStyles.service_name_prompt, completer=services)
+    decryption_password = prompt(CliStyles.decryption_pass_prompt)
+    try:
+        username, password = client.fetch_service(chosen_service, decryption_password)
+        print(
+            CliStyles.credentials_resp_format.format(chosen_service, username, password)
+        )
+        copy_to_clipboard(password)  # Add pass to clipboard for convenience
+    except TypeError:
+        print(CliStyles.password_invalid.format(decryption_password))
+    except ServiceNotFound:
+        print(CliStyles.service_not_found.format(chosen_service))
+
+
+if __name__ == "__main__":
+    client = CredentialsManager()
+    print(CliStyles.ascii_art)
+    while True:
+        action = prompt("")
+        print("\033[1A" + "\033[K", end="")  # Clear user input
+        if action.lower() == "help":
+            print(CliStyles.ascii_art)
+        if action.lower()[0] not in "fndleq":
+            print("Not a valid slection")
+            continue
+        match action.lower():
+            case "f":
+                fetch_service(client)
+            case "n":
+                pass
+            case "l":
+                pass
+            case "e":
+                pass
+            case "q":
+                break
--- src/lockin/styles.py
+++ src/lockin/styles.py
@@ -1,43 +1,75 @@
 from toga.style import Pack
-from toga.style.pack import COLUMN, CENTER 
+from toga.style.pack import COLUMN, CENTER
 
 
 class Styles:
     ENCRYPT_POPUP_WINDOW_STYLE = Pack(
-        direction=COLUMN, 
-        width=300, 
-        alignment=CENTER, 
-        padding_left=150
+        direction=COLUMN, width=300, alignment=CENTER, padding_left=150
     )
 
-    SAVE_BUTTON_STYLE = Pack(
-        padding_bottom=10, 
-        height=30
-    )
+    SAVE_BUTTON_STYLE = Pack(padding_bottom=10, height=30)
 
-    ENCRYPT_TEXT_INPUT_STYLE = Pack(
-        height=50,
-        padding_top=20,
-        padding_bottom=20
-    )
+    ENCRYPT_TEXT_INPUT_STYLE = Pack(height=50, padding_top=20, padding_bottom=20)
 
     DECRYPT_BUTTON_STYLE = Pack(
-        width=150,
-        padding_left=225,
-        padding_bottom=10,
-        height=30
+        width=150, padding_left=225, padding_bottom=10, height=30
     )
 
     DECRYPT_TEXT_INPUT_STYLE = Pack(
-        height=40,
-        width=290, 
-        padding_left=160, 
-        padding_top=20, 
-        padding_bottom=20
+        height=40, width=290, padding_left=160, padding_top=20, padding_bottom=20
     )
 
-    DECRYPT_LABEL_STYLE = Pack(
-        font_size=20, 
-        padding_top=20, 
-        padding_left=153
-    )
+    DECRYPT_LABEL_STYLE = Pack(font_size=20, padding_top=20, padding_left=153)
+
+
+class CliStyles:
+    ascii_art = r"""
+        __               __   ____    
+       / /   ____  _____/ /__/  _/___ 
+      / /   / __ \/ ___/ //_// // __ \
+     / /___/ /_/ / /__/ ,< _/ // / / /
+    /_____/\____/\___/_/|_/___/_/ /_/
+   ----------------------------------
+    
+    Fetch or F to fetch record
+    New or N to add a new record
+    Delete or D to delete a record
+    List or L to list all records
+    Edit or E to edit a record
+    """
+
+    credentials_resp_format = """
+    \n
+    <<<<<<<<<<-------------->>>>>>>>>>
+    Service: {}\n
+    Username: {}\n
+    Password: {}
+    <<<<<<<<<<-------------->>>>>>>>>>
+    \n
+    """
+
+    service_name_prompt = """
+    Enter Service Name: 
+    """
+
+    decryption_pass_prompt = """
+    Enter Decryption Password: 
+    """
+
+    service_not_found = """
+    ------------------------------\n
+    Error: Service '{}' not found.\n
+    ------------------------------\n
+    """
+
+    service_already_exists = """
+    ------------------------------\n
+    Error: Service '{}' already exists.\n
+    ------------------------------\n
+    """
+
+    password_invalid = """
+    ------------------------------\n
+    Error: Password '{}' is invalid.\n
+    ------------------------------\n
+    """
