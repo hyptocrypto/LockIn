5691106c3dc41a863d59dfa2740f891d537e5a35
Julian Baumgartner <jbaumgartner93@gmail.com>
2022-10-11 19:46:01 -0400

    Squash me daddy

--- src/lockin/smb.py
+++ src/lockin/smb.py
@@ -1,24 +1,41 @@
-from asyncio.proactor_events import _ProactorBaseWritePipeTransport
 import os
-from typing import Callable
 from settings import PASSWORD, NAS_HOST, NETWORK_SHARE_URI
 
 
 class SMBClient:
-    "Main connector for network volumes"
+    """
+    Main connector for network volumes
+    TODO: Add some edge case / error handling for if sys calls fail
+    """
 
     def __init__(self):
-        self.pre_connected = os.path.isdir(NETWORK_SHARE_URI)
+        self.pre_connected = os.path.ismount(NETWORK_SHARE_URI)
 
     def __enter__(self):
-        if not self.pre_connected:
+        """Check if network share is already mounted. If not mount the share"""
+        if self.pre_connected:
+            return self
+
+        # Create NETWORK_SHARE_URI dir if needed
+        if not os.path.isdir(NETWORK_SHARE_URI):
             os.system("echo %s|sudo -S %s" % (PASSWORD, f"mkdir {NETWORK_SHARE_URI}"))
-            os.system(
-                "echo %s|sudo -S %s"
-                % (PASSWORD, f"mount_smbfs {NAS_HOST} {NETWORK_SHARE_URI}")
-            )
+
+        # Mount share
+        os.system(
+            "echo %s|sudo -S %s"
+            % (PASSWORD, f"mount_smbfs {NAS_HOST} {NETWORK_SHARE_URI}")
+        )
+
         return self
 
-    def __exit__(self):
-        if not self.pre_connected:
-            os.system("echo %s|sudo -S %s" % (PASSWORD, f"umount {NETWORK_SHARE_URI}"))
+    def __exit__(self, *args):
+        """ "Check if network share was pre-mounted. If not, unmount"""
+        if self.pre_connected:
+            return
+        os.system("echo %s|sudo -S %s" % (PASSWORD, f"umount {NETWORK_SHARE_URI}"))
+
+
+# TESTING BLOCK
+if __name__ == "__main__":
+    with SMBClient() as cliient:
+        os.system("echo %s|sudo -S %s" % (PASSWORD, f"ls {NETWORK_SHARE_URI}"))
